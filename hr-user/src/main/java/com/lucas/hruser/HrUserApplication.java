4e805bedb2b432e253464da1766b30438972e2d4
Lucas França <lucasfreitas_c5@hotmail.com>
2022-12-19 18:34:24 -0300

    login e token jwt

--- hr-user/src/main/java/com/lucas/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/lucas/hruser/HrUserApplication.java
@@ -1,16 +1,24 @@
 package com.lucas.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		//System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+	}
 }
